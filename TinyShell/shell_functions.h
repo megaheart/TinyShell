5ce863d45a12105b5a3e52dde6ce20f240c6ad8f
megaheart <21360138+megaheart@users.noreply.github.com>
2022-05-20 18:25:08 +0700

    :)

--- TinyShell/shell_functions.h
+++ TinyShell/shell_functions.h
@@ -3,15 +3,11 @@
 #include <windows.h>
 #include<vector>
 #include<string>
-<<<<<<< HEAD
-=======
 typedef int (*ShellFunction)(TCHAR** cmdParts, int partCount);
 typedef struct ShellCommand {
     const TCHAR* name;
     ShellFunction func;
 } ShellCommand;
->>>>>>> master
-
 void initializeCmds();
 ShellCommand** getAllCommands(int &cmdsCount);
 int executeCommand(/*TCHAR* cmdLine, */TCHAR** cmdParts, int partCount);
