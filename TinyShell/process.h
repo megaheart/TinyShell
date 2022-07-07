5f7a47df8ff5bdf5a9f371950b56e736f7c16373
megaheart <21360138+megaheart@users.noreply.github.com>
2022-07-07 10:03:03 +0700

    :))

--- TinyShell/process.h
+++ TinyShell/process.h
@@ -7,8 +7,8 @@ typedef struct {
 	LPSTARTUPINFO si;
 	int status;
 }ProcessInfo;
-#define PROC_STAT_RUNNING 1;
-#define PROC_STAT_STOP 2;
-#define PROC_STAT_TERMINATED 3;
+#define PROC_STAT_RUNNING 1
+#define PROC_STAT_STOP 2
+#define PROC_STAT_TERMINATED 3
 std::vector<ProcessInfo> *getProcessInfos();
 int proc(TCHAR** cmdParts, int partCount);
